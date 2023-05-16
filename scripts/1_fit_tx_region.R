f3a15dc7fe555860554559772bde5e97a6fb1274
TimeWz667 <TimeWz667@gmail.com>
2023-05-16 15:43:23 +0100

    Add regional statistics

---
+++ scripts/1_fit_tx_region.R
@@ -0,0 +1,109 @@
+library(tidyverse)
+library(rstan)
+
+options(mc.cores = 4)
+rstan_options(auto_write = TRUE)
+
+
+## Data loading
+
+pop <- local({
+  locs <- dir("data")
+  locs <- locs[startsWith(locs, "targets_Region")]
+  locs <- gsub("targets_Region_", "", locs)
+  locs <- gsub(".csv", "", locs)
+  locs
+  
+  state_map <- read_csv(here::here("data", "StateMap.csv")) %>% 
+    mutate(
+      State = gsub("\\s+", "_", State_Pop),
+      State = gsub("&", "and", State)
+    )
+  
+  pop <- read_csv(here::here("data", "population.csv"))
+  pop <- pop %>% 
+    filter(Year == 2019) %>% 
+    filter(Sex == "Total") %>% 
+    mutate(
+      Location = gsub("\\s+", "_", Location),
+      Location = gsub("&", "and", Location)
+    ) %>% 
+    rename(State = Location) %>% 
+    left_join(state_map %>% select(State = State, Region)) %>% 
+    group_by(Region) %>% 
+    summarise(Pop = sum(Pop)) %>% 
+    filter(!is.na(Region))
+  
+  write_csv(pop, here::here("data", "population_region.csv"))
+
+  pop <- set_names(pop$Pop, pop$Region)[locs]
+  
+  pop
+})
+
+
+
+data_locs <- lapply(names(pop), function(loc) {
+  
+  targets <- read_csv(here::here("data", "targets_Region_" + glue::as_glue(loc) + ".csv"))
+  
+  drug <- targets %>% filter(Index == "DrugTime")
+  tx <- targets %>% filter(Index == "PrTxiPub") %>% 
+    mutate(X = round(M * N))
+  
+  det <- targets %>% 
+    filter(Year == 2019) %>% 
+    filter(Index == "TxI") %>% 
+    mutate(N_Txi = round(N * M))
+  
+  
+  ds <- list(
+    N_Txi_Pub = det$N_Txi[1],
+    N_Txi_Eng = det$N_Txi[2],
+    Pop = unname(pop[loc]),
+    Tx = tx$N,
+    Tx_Pub = tx$X,
+    Drug = drug$M,
+    Drug_Std = drug$Error,
+    ppv_pub = 0.75,
+    dur_upper = 1
+  )
+  
+})
+names(data_locs) <- names(pop)
+
+
+
+### Fitting ----
+
+src_model <- "tx_11.stan"
+model <- rstan::stan_model(here::here("stan", src_model))
+
+folder <- "tx_11_regional"
+dir.create(here::here("out", folder), showWarnings = F)
+
+for(loc in names(data_locs)) {
+  print(loc)
+  
+  ds <- data_locs[[loc]]
+  
+  
+  post <- rstan::sampling(model, data=ds, iter=5e3, warmup=4e3)
+  
+  tab <- as.data.frame(summary(post)$summary)
+  tab$Name <- rownames(tab)
+  tab <- tab %>% as_tibble() %>% relocate(Name)
+  
+  ext <- rstan::extract(post) %>% 
+    data.frame() %>% 
+    as.tibble() %>% 
+    mutate(
+      ppv_pub = ds$ppv_pub,
+      ent_pub = ds$entpub,
+      pdx_pub = ds$pdx_pub
+    )
+  
+  write_csv(tab, file = here::here("out", folder, "summary_" + glue::as_glue(loc) + ".csv"))
+  write_csv(ext, file = here::here("out", folder, "post_" + glue::as_glue(loc) + ".csv"))
+}
+
---
+++ scripts/3_vis_regional.R
@@ -0,0 +1,141 @@
+if ("rstan" %in% (.packages())) {
+  detach("package:rstan", unload = T)
+}
+library(tidyverse)
+library(tidybayes)
+
+theme_set(theme_bw() + theme(text = element_text(family = "sans")))
+
+
+load(file = here::here("docs", "tabs", "post_regional.rdata"))
+
+
+stats <- post %>% 
+  pivot_longer(-c(Region, Pop), names_to = "Index") %>% 
+  group_by(Region, Pop, Index) %>% 
+  summarise(
+    M = median(value),
+    L1 = quantile(value, 0.025),
+    U1 = quantile(value, 0.975),
+    L2 = quantile(value, 0.25),
+    U2 = quantile(value, 0.75),
+    U = U1,
+    L = L1
+  ) %>% 
+  ungroup()
+
+
+
+stats %>% 
+  filter(Index == "p_under") %>% 
+  mutate(
+    Region = reorder(Region, M)
+  ) %>% 
+  ggplot() +
+  geom_pointinterval(aes(x = M, xmin = L1, xmax = U1, y = Region)) +
+  geom_errorbar(aes(x = M, xmin = L2, xmax = U2, y = Region), linewidth = rel(0.6), width = 0.6) +
+  geom_errorbar(aes(x = M, xmin = L1, xmax = U1, y = Region), linewidth = rel(0.3), width = 0.25)
+
+
+
+g_dur_pri <- stats %>% 
+  filter(Index == "dur_pri") %>% 
+  mutate(
+    Region = reorder(Region, M)
+  ) %>% 
+  ggplot() +
+  geom_pointinterval(aes(x = M, xmin = L1, xmax = U1, y = Region)) + 
+  scale_x_continuous("Treatment duration, on private drug, month", labels = scales::number_format(scale = 12)) +
+  expand_limits(x = 0)
+
+
+g_dur_pri
+
+
+g_ppv_pri <- stats %>% 
+  filter(Index == "ppv_pri") %>% 
+  mutate(
+    Region = reorder(Region, M)
+  ) %>% 
+  ggplot() +
+  geom_pointinterval(aes(x = M, xmin = L1, xmax = U1, y = Region)) + 
+  scale_x_continuous("Positive Predictive Value, in the private sector, %", labels = scales::percent_format()) + 
+  expand_limits(x = c(0, 1))
+
+g_ppv_pri
+
+
+g_drug <- post %>% 
+  mutate(
+    Region = reorder(Region, tp_pri_drug / Pop)
+  ) %>% 
+  ggplot() +
+  stat_halfeye(aes(x = tp_pri_drug / Pop, y = Region)) +
+  #geom_pointinterval(aes(x = M / Pop, xmin = L / Pop, xmax = U / Pop, y = State), size = rel(2)) + 
+  scale_x_continuous("Treatment initiated with private drugs, per 100,000", 
+                     labels=scales::number_format(scale = 1e5), limits = c(0, 2e-3)) + 
+  expand_limits(x = 0)
+
+g_drug
+
+
+g_drugT <- post %>% 
+  mutate(
+    Region = reorder(Region, tp_pri_drug_time)
+  ) %>% 
+  ggplot() +
+  stat_halfeye(aes(x = tp_pri_drug_time, y = Region)) +
+  # geom_pointinterval(aes(x = M, xmin = L, xmax = U, y = State)) + 
+  scale_x_continuous("Caseloads with private drugs, millions", labels=scales::number_format(scale = 1e-6)) + 
+  expand_limits(x = c(0, 1))
+
+g_drugT
+
+
+
+g_txi <- post %>% 
+  mutate(
+    Region = reorder(Region, tp_pri_txi / Pop)
+  ) %>% 
+  ggplot() +
+  stat_halfeye(aes(x = tp_pri_txi / Pop, y = Region)) +
+  scale_x_continuous("Unreported private cases, per 100,000", labels=scales::number_format(scale = 1e5), limits = c(0, 2e-3)) + 
+  expand_limits(x = 0)
+
+g_txi
+
+
+g_under <- post %>% 
+  mutate(
+    Region = reorder(Region, p_under)
+  ) %>% 
+  ggplot() +
+  stat_halfeye(aes(x = p_under, y = Region)) +
+  scale_x_continuous("Unreported private cases / All cases initiated treatment, %", labels=scales::percent) + 
+  expand_limits(x = 0:1)
+
+g_under
+
+
+g_priunder <- post %>% 
+  mutate(
+    Region = reorder(Region, 1 - ppm)
+  ) %>% 
+  ggplot() +
+  stat_halfeye(aes(x = 1 - ppm, y = Region)) +
+  #geom_pointinterval(aes(x = M, xmin = L2, xmax = U2, y = State)) + 
+  scale_x_continuous("Unreported private cases / All private cases initiated treatment, %", labels=scales::percent) + 
+  expand_limits(x = 0:1)
+
+g_priunder
+
+
+ggsave(g_ppv_pri, filename = here::here("docs", "figs", "g_rg_ppv.png"), width = 6, height = 5)
+ggsave(g_dur_pri, filename = here::here("docs", "figs", "g_rg_dur.png"), width = 6, height = 5)
+ggsave(g_drug, filename = here::here("docs", "figs", "g_rg_drug.png"), width = 6, height = 5)
+ggsave(g_drugT, filename = here::here("docs", "figs", "g_rg_drugt.png"), width = 6, height = 5)
+ggsave(g_txi, filename = here::here("docs", "figs", "g_rg_txi.png"), width = 6, height = 5)
+ggsave(g_under, filename = here::here("docs", "figs", "g_rg_under.png"), width = 6, height = 5)
+ggsave(g_priunder, filename = here::here("docs", "figs", "g_rg_priunder.png"), width = 6, height = 5)
+
+
